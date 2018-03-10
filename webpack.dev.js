fa9d6f16335c4f4f0cb6ef4291157fe4201d9757
Gunnar Örn Baldursson <Gunso_Baldurs@hotmail.com>
2018-03-10 15:39:05 +0000

    INIT
    Finished setting up project

---
+++ webpack.dev.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Styled Components',
+            template: path.join(__dirname, 'index.html'),
+            inject: 'body'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        open: true,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'css-loader',
+                    options: {
+                        modules: true,
+                        localIdentName: '[name]__[local]__[hash:base64:5]'
+                    }
+                }
+            }
+        ]
+    }
+}
